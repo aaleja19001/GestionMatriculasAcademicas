2a167667f1ef8420cbd1bd463f951350aac5bd2e
aaleja19001 <ddagmaaralejandra@gmail.com>
2026-02-26 18:37:24 -0500

    proyecto inicial

---
+++ src/main/java/com/ale/edu/gestionmatriculasacademicas/GestionMatriculasAcademicasApplication.java
@@ -0,0 +1,13 @@
+package com.ale.edu.gestionmatriculasacademicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionMatriculasAcademicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionMatriculasAcademicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ale/edu/gestionmatriculasacademicas/GestionMatriculasAcademicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ale.edu.gestionmatriculasacademicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionMatriculasAcademicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
