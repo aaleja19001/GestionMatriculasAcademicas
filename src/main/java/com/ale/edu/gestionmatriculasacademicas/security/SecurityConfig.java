281922f7291e79a77e15e0eb53429fdc1318c998
aaleja19001 <ddagmaaralejandra@gmail.com>
2026-04-18 14:42:44 -0500

    configurando pruebas de despliegue

--- src/main/java/com/ale/edu/gestionmatriculasacademicas/security/SecurityConfig.java
+++ src/main/java/com/ale/edu/gestionmatriculasacademicas/security/SecurityConfig.java
@@ -63,7 +63,7 @@ private String allowedOrigins;
 @Bean
 public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of(allowedOrigins));
+    configuration.setAllowedOrigins(List.of(allowedOrigins.split(",")));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
