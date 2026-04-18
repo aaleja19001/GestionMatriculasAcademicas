e26a217be6cf77421996d9e9a6a0d5721927f3e0
aaleja19001 <ddagmaaralejandra@gmail.com>
2026-04-18 11:48:34 -0500

    se configura url para despliegue

--- src/main/java/com/ale/edu/gestionmatriculasacademicas/security/SecurityConfig.java
+++ src/main/java/com/ale/edu/gestionmatriculasacademicas/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.ale.edu.gestionmatriculasacademicas.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -57,18 +58,19 @@ public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http.build();
 }
 
+@Value("${cors.allowed-origins}")
+private String allowedOrigins;
 @Bean
 public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+    configuration.setAllowedOrigins(List.of(allowedOrigins));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
-    return source -> {
-        UrlBasedCorsConfigurationSource src = new UrlBasedCorsConfigurationSource();
-        src.registerCorsConfiguration("/**", configuration);
-        return src.getCorsConfiguration(source);
-    };
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
 }
 
     @Bean
