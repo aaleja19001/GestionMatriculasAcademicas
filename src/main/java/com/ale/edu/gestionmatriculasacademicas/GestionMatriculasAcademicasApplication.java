bb85d84a0f2dba90e46395b7c5ffd8cc02f9f130
aaleja19001 <ddagmaaralejandra@gmail.com>
2026-05-03 23:26:33 -0500

    test credenciales BD

--- src/main/java/com/ale/edu/gestionmatriculasacademicas/GestionMatriculasAcademicasApplication.java
+++ src/main/java/com/ale/edu/gestionmatriculasacademicas/GestionMatriculasAcademicasApplication.java
@@ -1,5 +1,6 @@
 package com.ale.edu.gestionmatriculasacademicas;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,12 @@ public class GestionMatriculasAcademicasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionMatriculasAcademicasApplication.class, args);
+	}
+
+	@PostConstruct
+	public void debugAll() {
+		System.out.println("ENV USER: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
+		System.out.println("PROP USER: " + System.getProperty("spring.datasource.username"));
 	}
 
 }
